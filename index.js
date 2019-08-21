e8ad5f74279d17292efac1b3cf59d1c43cf15ed9
Alejandro Mejia <aalejandro.mreyes@icloud.com>
2019-08-20 21:14:59 -0400

    Adds GET, POST, PUT, DELETE endpoints

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
+const db = require("./data/hubs-model");
+
 const server = express();
+
+// middleware
+server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send("Hello word");
@@ -8,6 +13,69 @@ server.get("/", (req, res) => {
 
 server.get("/now", (req, res) => {
   res.send(Date());
+});
+
+server.get("/hubs", (req, res) => {
+  db.find()
+    .then(hubs => {
+      res.json(hubs);
+      // especificly says we are returning json
+    })
+    .catch(error => {
+      res.status(500).json({
+        error
+      });
+    });
+});
+
+server.delete("/hubs/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.remove(id)
+    .then(hub => {
+      hub
+        ? res.status(200).json(hub)
+        : res.status(404).json({
+            error: "Hub not found"
+          });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error
+      });
+    });
+});
+
+server.put("/hubs/:id", (req, res) => {
+  const { id } = req.params;
+  const newHub = req.body;
+  db.update(id, newHub)
+    .then(hub => {
+      hub
+        ? res.status(200).json(hub)
+        : res.status(404).json({
+            error: "Hub not found"
+          });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error
+      });
+    });
+});
+
+server.post("/", (req, res) => {
+  const newHub = req.body;
+  db.add(newHub)
+    .then(hub => {
+      res.status(201).json(hub);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error,
+        message: "failed to create new hub"
+      });
+    });
 });
 
 server.listen(4000, () => {
